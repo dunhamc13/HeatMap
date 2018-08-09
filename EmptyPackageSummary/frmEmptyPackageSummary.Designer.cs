e80f1f17dee3fb03145f6dc0cb93b6dc440d30b6
OlympianCode <johnmcmahon427@gmail.com>
2018-08-08 17:04:49 -0700

    progress made, now reads and interprets csvs, some results visible

--- EmptyPackageSummary/frmEmptyPackageSummary.Designer.cs
+++ EmptyPackageSummary/frmEmptyPackageSummary.Designer.cs
@@ -28,20 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnView = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.grpAreas = new System.Windows.Forms.GroupBox();
+            this.dgvAreas = new System.Windows.Forms.DataGridView();
+            this.Area = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Count = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.grpAreas.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAreas)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(369, 10);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(134, 23);
+            this.btnOpen.TabIndex = 3;
+            this.btnOpen.Text = "&Analyze Report...";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.AllowDrop = true;
+            this.txtFilePath.Location = new System.Drawing.Point(12, 12);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(351, 20);
+            this.txtFilePath.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Comma Separated Values|*.csv";
+            // 
+            // btnView
+            // 
+            this.btnView.Enabled = false;
+            this.btnView.Location = new System.Drawing.Point(509, 10);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(100, 23);
+            this.btnView.TabIndex = 4;
+            this.btnView.Text = "&View Report...";
+            this.btnView.UseVisualStyleBackColor = true;
+            this.btnView.Click += new System.EventHandler(this.btnView_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(534, 562);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "C&lear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // grpAreas
+            // 
+            this.grpAreas.Controls.Add(this.dgvAreas);
+            this.grpAreas.Location = new System.Drawing.Point(12, 39);
+            this.grpAreas.Name = "grpAreas";
+            this.grpAreas.Size = new System.Drawing.Size(285, 420);
+            this.grpAreas.TabIndex = 7;
+            this.grpAreas.TabStop = false;
+            this.grpAreas.Text = "Top Areas";
+            // 
+            // dgvAreas
+            // 
+            this.dgvAreas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAreas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Area,
+            this.Count});
+            this.dgvAreas.Location = new System.Drawing.Point(6, 19);
+            this.dgvAreas.Name = "dgvAreas";
+            this.dgvAreas.Size = new System.Drawing.Size(273, 395);
+            this.dgvAreas.TabIndex = 0;
+            // 
+            // Area
+            // 
+            this.Area.HeaderText = "Area";
+            this.Area.Name = "Area";
+            this.Area.ReadOnly = true;
+            // 
+            // Count
+            // 
+            this.Count.HeaderText = "Count";
+            this.Count.Name = "Count";
+            this.Count.ReadOnly = true;
             // 
             // frmEmptyPackageSummary
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(621, 597);
+            this.Controls.Add(this.grpAreas);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnView);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.txtFilePath);
             this.Name = "frmEmptyPackageSummary";
             this.Text = "Empty Package Summary";
+            this.grpAreas.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAreas)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnView;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.GroupBox grpAreas;
+        private System.Windows.Forms.DataGridView dgvAreas;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Area;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Count;
     }
 }
 
--- EmptyPackageSummary/frmEmptyPackageSummary.cs
+++ EmptyPackageSummary/frmEmptyPackageSummary.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,112 @@ namespace EmptyPackageSummary
 {
     public partial class frmEmptyPackageSummary : Form
     {
+        string[] lines;
+        string path = "";
+        string[][] data;
         public frmEmptyPackageSummary()
         {
             InitializeComponent();
         }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            //ResetForm();
+            openFileDialog1.ShowDialog();
+            path = openFileDialog1.FileName;
+            txtFilePath.Text = path;
+            OpenFile(txtFilePath.Text);
+            if (path.Length > 0)
+            {
+                btnView.Enabled = true;
+            }
+        }
+
+        private void OpenFile(string path)
+        {
+            try
+            {
+                lines = System.IO.File.ReadAllLines(path); // get file into string array
+            }
+            catch (ArgumentException ae)
+            {
+                //don't do anything, stop processing
+                return;
+            }
+            data = new string[lines.Length - 1][];
+            for (int i = 1; i < lines.Length; i++)
+            {
+                //https://forums.asp.net/t/1247607.aspx?Reading+CSV+with+comma+placed+within+double+quotes+
+                // extract the fields
+                Regex CSVParser = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+                String[] thisRow = CSVParser.Split(lines[i]);
+                // clean up the fields (remove " and leading spaces)
+                for (int j = 0; j < thisRow.Length; j++)
+                {
+                    thisRow[j] = thisRow[j].TrimStart(' ', '"');
+                    thisRow[j] = thisRow[j].TrimEnd('"');
+                }
+                data[i - 1] = thisRow;
+            }
+            AnalyzeData();
+        }
+
+        private void AnalyzeData()
+        {
+            DateTime earliest = DateTime.Parse(data[0][7]);
+            DateTime latest = DateTime.Parse(data[0][7]);
+            decimal totalDollars = 0m;
+            Dictionary<string, int> topLocations = new Dictionary<string, int>();
+            decimal percentWithoutLocation = 0m;
+            Dictionary<string, int> topDepartments = new Dictionary<string, int>();
+            for (int i = 0; i < data.Length; i++)
+            {
+                string thisLocation = data[i][6];
+                string thisDepartment = data[i][1];
+                decimal thisCost = decimal.Parse(data[i][4], System.Globalization.NumberStyles.Currency);
+                DateTime thisDate = DateTime.Parse(data[0][7]);
+
+                //check date
+                if (thisDate < earliest)
+                    earliest = thisDate;
+                else if (thisDate > latest)
+                    latest = thisDate; 
+
+                //accumulate the cost
+                totalDollars += thisCost;
+                //grab this location 
+                try
+                {
+                    //try to add to the count in the dictionary
+                    topLocations[thisLocation] += 1;
+                }
+                catch (KeyNotFoundException knf)
+                {
+                    //if it's not there, put it in the dictionary
+                    topLocations.Add(thisLocation, 1);
+                }
+                //same with department
+                try
+                {
+                    topDepartments[thisDepartment] += 1;
+                }
+                catch (KeyNotFoundException knf)
+                {
+                    topDepartments.Add(thisDepartment, 1);
+                }
+            }
+            List<KeyValuePair<string, int>> sortedLocations = topLocations.ToList<KeyValuePair<string, int>>();
+            sortedLocations.Sort((a, b) => b.Value.CompareTo(a.Value));
+            for (int i = 0; i < 20; i++)
+            {
+                dgvAreas.Rows.Add(sortedLocations[i].Key, sortedLocations[i].Value);
+            }
+        }
+
+        private void btnView_Click(object sender, EventArgs e)
+        {
+            Process.Start(path);
+        }
+
     }
 }
